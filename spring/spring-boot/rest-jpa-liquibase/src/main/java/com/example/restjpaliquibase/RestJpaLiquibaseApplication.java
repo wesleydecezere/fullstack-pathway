338fbb60cfd2cbbcfc08b39dc074efe9fc915e33
wesleydecezere <wesleydecezere@gmail.com>
2021-11-24 14:12:05 -0300

    Initial commit

---
+++ spring/spring-boot/rest-jpa-liquibase/src/main/java/com/example/restjpaliquibase/RestJpaLiquibaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.restjpaliquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestJpaLiquibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestJpaLiquibaseApplication.class, args);
+	}
+
+}

0cfc5336c1dc637af0de0a41a77b79acdd121a50
wesleydecezere <wesleydecezere@gmail.com>
2021-11-25 15:55:22 -0300

    Add Customer model and repository

---
+++ spring/spring-boot/rest-jpa-liquibase/src/main/java/com/example/restjpaliquibase/model/Customer.java
@@ -0,0 +1,68 @@
+package com.example.restjpaliquibase.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "customers")
+public class Customer extends ModelEntity{
+	private String name;
+	private String surname;
+	private String address;
+	private Date dateOfBirth;
+	private int numberOfPurchases;
+	@Column(columnDefinition="Decimal(10,2)")
+	private double amountSpent;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public int getNumberOfPurchases() {
+		return numberOfPurchases;
+	}
+
+	public void setNumberOfPurchases(int numberOfPurchases) {
+		this.numberOfPurchases = numberOfPurchases;
+	}
+
+	public double getAmountSpent() {
+		return amountSpent;
+	}
+
+	public void setAmountSpent(double amountSpent) {
+		this.amountSpent = amountSpent;
+	}
+}

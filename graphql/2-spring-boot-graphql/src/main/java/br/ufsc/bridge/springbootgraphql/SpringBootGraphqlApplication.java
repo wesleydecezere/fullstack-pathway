1fb7b2bb3486c43f776aa75e882f4a1d6588388b
wesleydecezere <wesleydecezere@gmail.com>
2022-01-10 10:40:07 -0300

    Initial commit

---
+++ graphql/2-spring-boot-graphql/src/main/java/br/ufsc/bridge/springbootgraphql/SpringBootGraphqlApplication.java
@@ -0,0 +1,13 @@
+package br.ufsc.bridge.springbootgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGraphqlApplication.class, args);
+	}
+
+}
